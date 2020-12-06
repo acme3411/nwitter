1e8dd56409109f7a49285614c5320da6369a05ab
Aaron <skylily@gmail.com>
2020-12-07 06:15:35 +0900

    2nd

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import "fireabse/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID
+  };
+  // Initialize Firebase
+  export default firebase.initializeApp(firebaseConfig);
